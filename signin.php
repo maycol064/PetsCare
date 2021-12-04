a06ae9d91cba7032fee9648f9d91d7cd7cd57704
Maycol Flores <maycolfloresruiz06@gmail.com>
2021-12-03 20:49:32 -0600

    Se modificaron rutas

--- signin.php
+++ signin.php
@@ -9,18 +9,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Roboto:wght@900&display=swap" rel="stylesheet">
-    <title>PetsCare</title>
+    <title>Pets Care</title>
 </head>
 <body class="body-bg">
-    <div class="login">
-        <h1 class="title">Pets Care</h1>
-        <img src="./assets/veterinarioB.png" class="icon-login">
-        <form action="./main.php" method="post" class="form-login">
-            <input type="text" class="fields-login user" placeholder="Usuario">
-            <input type="password" class="fields-login password" placeholder="Contraseña">
-            <button type="submit" class="submit">Entrar</button>
-        </form>
-        <a href="./signin.php" class="signin-button">Registrar Clínica</a>
-    </div>
+    
 </body>
 </html>
